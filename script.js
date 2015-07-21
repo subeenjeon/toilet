2f28231a07dc64ac09d8f93a5d054ed8470571a1
subeenjeon <powersubeen@naver.com>
2015-07-21 15:25:04 +0900

    d
    d

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ $(document).ready(function(){
 	$("#information_B").click(function(){
 		showInformation_C();
 	});
-
 	$("#information_C").click(function(){
+		showQ_01();
+	});
+	$("#Q_01").click(function(){
+		showQ_select01();
+	});
+	$("#Q_select01").click(function(){
 		backToInformation_A();
 	});
 });
@@ -20,7 +25,27 @@ var showInformation_A = function() {
 	$("#information_A").fadeIn();
 };
 
-var backToInformation_A = function() {
+var showInformation_B = function() {
+	$("#information_A").fadeOut();
+	$("#information_B").fadeIn();
+};
+
+var showInformation_C = function() {
+	$("#information_B").fadeOut();
+	$("#information_C").fadeIn();
+};
+
+var showQ_01 = function() {
 	$("#information_C").fadeOut();
+	$("#Q_01").fadeIn();
+};
+
+var showQ_select01 = function() {
+	$("#Q_01").fadeOut();
+	$("#Q_select01").fadeIn();
+};
+
+var backToInformation_A = function() {
+	$("#Q_select01").fadeOut();
 	$("#information_A").fadeIn();
 };
